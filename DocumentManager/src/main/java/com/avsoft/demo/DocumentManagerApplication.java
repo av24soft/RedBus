db80f33d22b5e7f312c65a27d798d45cbfb31fca
avsoft <av24soft@gmail.com>
2024-10-25 08:05:20 +0530

    changes are done as per review comment

--- DocumentManager/src/main/java/com/avsoft/demo/DocumentManagerApplication.java
+++ DocumentManager/src/main/java/com/avsoft/demo/DocumentManagerApplication.java
@@ -9,7 +9,7 @@ public class DocumentManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentManagerApplication.class, args);
 
-		sSystem.out.println("sds")
+		
 	}
 
 }
